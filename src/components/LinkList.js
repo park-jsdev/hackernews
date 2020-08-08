c3d48ad66ecad8121baf70e6112007ed6ccd4947
Terry Junsoo Park <58895710+park-jsdev@users.noreply.github.com>
2020-08-08 16:40:16 -0700

    added real time links and votes

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -33,6 +33,54 @@ export const FEED_QUERY = gql`
   }
 `
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`
+
 // Render links that are fetched from server from <Query /> render prop function
 // Wrap the returned code with <Query /> component passing FEED_QUERY as prop
 // Return linksToRender due to render prop function provided by <Query /> component
@@ -45,12 +93,68 @@ export const FEED_QUERY = gql`
 //  3. data: This is the actual data that was received from the server. It has the links property which
 // represents a list of Link elements
 class LinkList extends Component {
+  // read current state of the cached data for the FEED_QUERY from the store
+  // Now you’re retrieving the link that the user just voted for from that list. You’re also manipulating 
+  // that link by resetting its votes to the votes that were just returned by the server.
+  // Finally, you take the modified data and write it back into the store.
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY })
+  
+    const votedLink = data.feed.links.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+  
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
+
+  // Passing 2 arguments to subscribeToMore:
+  // 1. document: This represents the subscription query itself. In your case, the subscription will fire every time a new link is created.
+  // 2. updateQuery: Similar to cache update prop, this function allows you to determine how the store should be updated with the information 
+  // that was sent by the server after the event occurred. In fact, it follows exactly the same principle as a Redux reducer: It takes as arguments 
+  // the previous state (of the query that subscribeToMore was called on) and the subscription data that’s sent by the server. You can then determine 
+  // how to merge the subscription data into the existing state and return the updated data. All you’re doing inside updateQuery is retrieving the new 
+  // link from the received subscriptionData, merging it into the existing list of links and returning the result of this operation.
+  _subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev
+        const newLink = subscriptionData.data.newLink
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+        if (exists) return prev;
+  
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        })
+      }
+    })
+  }
+
+  // Subscribe to new votes that are submitted by other users so that the latest vote count is always visible in app
+  // similar to subscribeToLinks but now using NEW_VOTES_SUBSCRIPTION as document.
+  // You pass in a subscription that asks for newly created votes.
+  // When the subscription fires, Apollo Client automatically updates the link that was voted on.
+  _subscribeToNewVotes = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION
+    })
+  }
+
   render() {
     return (
         <Query query={FEED_QUERY}>
-          {({ loading, error, data }) => {
+          {({ loading, error, data, subscribeToMore }) => {
             if (loading) return <div>Fetching</div>
             if (error) return <div>Error</div>
+            // using subscribeToMore received as prop into component's render prop function
+            // Calling _subscribeToNewLinks with its respective subscribeToMore function you make sure that
+            // the component actually subscribes to the events.
+            // This call opens up a websocket connection to the subscription server.
+            this._subscribeToNewLinks(subscribeToMore)
+            this._subscribeToNewVotes(subscribeToMore)
       
             const linksToRender = data.feed.links
       
@@ -69,18 +173,6 @@ class LinkList extends Component {
           }}
         </Query>
       )
-  }
-  // read current state of the cached data for the FEED_QUERY from the store
-  // Now you’re retrieving the link that the user just voted for from that list. You’re also manipulating 
-  // that link by resetting its votes to the votes that were just returned by the server.
-  // Finally, you take the modified data and write it back into the store.
-  _updateCacheAfterVote = (store, createVote, linkId) => {
-    const data = store.readQuery({ query: FEED_QUERY })
-  
-    const votedLink = data.feed.links.find(link => link.id === linkId)
-    votedLink.votes = createVote.link.votes
-  
-    store.writeQuery({ query: FEED_QUERY, data })
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BrowserRouter } from 'react-router-dom'
 import { setContext } from 'apollo-link-context'
 import { AUTH_TOKEN } from './constants'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
 // GraphQL need 2 API layers in the backend. Prisma provides the database layer which offers CRUD operations
 // the 2nd layer is the application layer for business logic
@@ -41,9 +44,41 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-// Instantiate ApolloClient by passing in httpLink and new instance of an InMemoryCache
+// Configure the ApolloClient with info about the subscriptions endpoint.
+// This is done by adding another ApolloLink to the Apollo middleware chain.
+// Use WebSocketLink from the apollo-link-ws package.
+// We instantiate a WebSocketLink that knows the subscriptions endpoint.
+// We use ws instead of http protocol. We also authenticate using the websocket
+// connection with the user's token from localStorage
+
+// Create a new WebSocketLink that represents the WebSocket connection
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+// Use split for proper "routing" of the requests and update the constructor call of ApolloClient
+// split takes three arguments: test function that returns a boolean. Other two are type ApolloLink.
+// if test returns true, the request will be forwarded to the link passed as the second argument.
+// If false, to the third one.
+// In this case, test is checking whether the request operation is a subscription. If so, it will be
+// forwarded to the wsLink, otherwise (if its a query or mutation), the authLink.concat(httpLink) will handle it.
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink), // make sure ApolloClient gets instantiated with correct link
+  link,
   cache: new InMemoryCache()
 })
 
